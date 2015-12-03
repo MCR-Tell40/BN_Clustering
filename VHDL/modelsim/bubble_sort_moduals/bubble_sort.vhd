58176e495eae9650f633f6b0db5b7f37f8994796
Nicholas Mead <n1ck.mead@yahoo.co.uk>
2015-12-03 15:28:14 +0000

    merge conflict, nicks mac was out of date

--- VHDL/modelsim/bubble_sort_moduals/bubble_sort.vhd
+++ VHDL/modelsim/bubble_sort_moduals/bubble_sort.vhd
@@ -15,43 +15,6 @@ ENTITY BubbleSort IS
 
   PORT(
 
-<<<<<<< HEAD
-    dataIn      		: in 	dataTrain;
-    beginSortValid	: in 	std_logic; -- not convinsed this is needed
-
-    dataOut   			: out dataTrain;
-    switchMadeValid : out std_logic;
-    switchMadeReset : in 	std_logic
-    );
-end BubbleSort;
-
--------------------------------------------------------------------------------
----------------------- Even Achitecture ----------------------------------------
-
-architecture even of BubbleSort is
-	-- reset patterns
-	constant reset_patten_spp 	: std_logic_vector(29 down to 0) := (others => '0');
-	constant reset_patten_train : dataTrain := (others => reset_patten_spp);
-begin
-	--
-	process(clk, rst)
-	begin
-		if rst = '1' then
-			-- Clear output
-			dataOut <= reset_patten_train;
-		
-		elsif rising_edge(clk) then 
-			-- itterate over data train
-			for i in (0 to 98) loop
-				-- check even
-				if (i mod 2 = '0') then
-					-- check if switch is required
-					if (makeSwitch(dataIn(i),dataIn(i+1)) = '1') then
-						-- make switch
-						dataOut(i) 		<= dataIn(i+1);
-						dataOut(i+1) 	<= dataIn(i);
-						switchMadeValid <= '1';
-=======
    	rst 			: in 	std_logic;	
 
    	dataIn      	: in 	dataTrain;
@@ -61,17 +24,10 @@ begin
   	dataOut   		: out 	dataTrain
   );
 
--------------------------------------------------------------------------------
----------------------- Odd Achitecture ----------------------------------------
-
-architecture odd of BubbleSort is
-=======
 ARCHITECTURE a OF BubbleSort IS
->>>>>>> 04b58fd990210acc573b1e7dfa7f87328b6474ac
 	-- reset patterns
 	constant reset_patten_spp 	: std_logic_vector(29 downto 0) := (others => '0');
 	constant reset_patten_train : dataTrain := (others => reset_patten_spp);
-
 		
 BEGIN
 
